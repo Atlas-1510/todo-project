f56bdff503682846bfd3515fe2c58a1cfdc45ab6
Jason Aravanis <j.aravanis@icloud.com>
2021-01-02 10:26:31 +1100

    - Setting up backend layout - Implemented Map object for storing user data - Created functions for creating, rendering, storing user tasks - Began to split into 'main' and 'render' modules

--- src/modules/app.js
+++ src/modules/app.js
@@ -7,26 +7,58 @@ import trashIcon from "../img/trash.svg"
 
 export const runApp = () => {
 
+    const main = (() => {
+        // Renders a task object to the DOM
+        function renderTask(completeBool, title, dueDate) {
+            const taskNode = createNode("li", userContentContainer, "", "task")
+            const checkbox = createNode("img", taskNode, "", "checkbox")
+            if (!completeBool) {
+                checkbox.src = emptySquareIcon
+            } else {
+                checkbox.src = filledSquareIcon
+            }
+            const taskDescription = createNode("div", taskNode, "", "taskDescription")
+            taskDescription.textContent = title
+            const taskDueDate = createNode("div", taskNode, "", "taskDueDate")
+            taskDueDate.textContent = dueDate
+            const editTaskIcon = createNode("img", taskNode, "", "editTaskIcon")
+            editTaskIcon.src = pencilSquareIcon
+            const deleteTaskIcon = createNode("img", taskNode, "", "deleteTaskIcon")
+            deleteTaskIcon.src = trashIcon
 
-
-    function renderTask(completeBool, title, dueDate) {
-        const taskNode = createNode("li", userContentContainer, "", "task")
-        const checkbox = createNode("img", taskNode, "", "checkbox")
-        if (!completeBool) {
-            checkbox.src = emptySquareIcon
-        } else {
-            checkbox.src = filledSquareIcon
+            return taskNode
         }
-        const taskDescription = createNode("div", taskNode, "", "taskDescription")
-        taskDescription.textContent = title
-        const taskDueDate = createNode("div", taskNode, "", "taskDueDate")
-        taskDueDate.textContent = dueDate
-        const editTaskIcon = createNode("img", taskNode, "", "editTaskIcon")
-        editTaskIcon.src = pencilSquareIcon
-        const deleteTaskIcon = createNode("img", taskNode, "", "deleteTaskIcon")
-        deleteTaskIcon.src = trashIcon
 
-        return taskNode
-    }
-    renderTask(false, "Test Title Here", "31/1/12")
+        // At the moment can add tasks, but only to DOM. Not actually saving in the backend anywhere.
+        // So the next thing to add is an ability to store task information, and then retrieve that info for display in DOM.
+        // To store task info, I can use a MAP object.
+
+        // Creates a task object
+        function createTask(completeBool, title, dueDate) {
+            const task = {
+                completeBool,
+                title,
+                dueDate,
+            }
+            return task
+        }
+
+        // Stores user tasks
+        const userData = new Map()
+
+        userData.set('1', createTask(false, "testTitle3", "testDueDate3"))
+
+        // Renders userData Map to DOM
+        function renderUserContent(map) {
+            for (const entry of map) {
+                const task = entry[1]
+                renderTask(task.completeBool, task.title, task.dueDate)
+            }
+        }
+
+        return { renderTask, createTask, renderUserContent, userData }
+    })()
+
+
+    main.renderUserContent(main.userData)
 }
